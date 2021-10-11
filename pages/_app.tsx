f7b6b8cd1a024b552aa11f1daf51de5420149b1e
Noah Litvin <noah.litvin@gmail.com>
2021-10-11 14:39:13 -0400

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 export default MyApp
