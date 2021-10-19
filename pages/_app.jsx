281bdf6f7d92ccfda3f877eb1af650d8174df41a
Noah Litvin <noah.litvin@gmail.com>
2021-10-19 14:09:30 +0100

    instal metamask notice

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,21 @@
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, Text } from "@chakra-ui/react";
 import Fonts from "../components/Fonts";
 import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
+  const noMetaMask = typeof web3 == "undefined";
+
   return (
     <ChakraProvider>
       <Fonts />
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Component {...pageProps} />
+      {noMetaMask ? (
+        <Text textAlign="center" py={40} fontWeight="bold" fontSize="3xl">
+          Install MetaMask!
+        </Text>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </ChakraProvider>
   );
 }
